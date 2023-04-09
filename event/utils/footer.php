24ad970eacc57c7a418b574b713334206190e76c
satyam rai <satyamrai200311@gmail.com>
2023-04-10 00:31:36 +0530

    Add files via upload

---
+++ event/utils/footer.php
@@ -0,0 +1,37 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>T</strong><span class = "small footerSubtext">eam</span>
+                        <strong>4</strong><span class = "small footerSubtext">04</span>
+                        <strong>F</strong><span class = "small footerSubtext">ound</span>
+                       
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        ************************<br>*************
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    404 Found<br>
+                    Creative Computing 2015-2016.<br>
+                   
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
